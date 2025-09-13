d957d3488b268dbad1295244666c71215523e289
Matthew Ballance <matt.ballance@gmail.com>
2025-09-13 17:18:52 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/pytest_dfm/impl/ext_rgy.py
+++ src/pytest_dfm/impl/ext_rgy.py
@@ -1,15 +1,11 @@
 
-import dataclasses as dc
 from dv_flow.mgr import ExtRgy as DfmExtRgy
 
-@dc.dataclass
 class ExtRgy(DfmExtRgy):
-    ext_rgy : DfmExtRgy = dc.field(default=None)
-    pkg_m : dict = dc.field(default_factory=dict)
 
-    def __post_init__(self):
-        super().__post_init__()
+    def __init__(self):
         self.ext_rgy = DfmExtRgy.inst()
+        self.pkg_m = {}
 
     def addPackage(self, name, pkgfile):
         self.pkg_m[name] = pkgfile
